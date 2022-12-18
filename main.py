8463ca06fd8f556dbe5f7f43ac9e237cf7b44ffb
FonderElite <s1170044@usls.edu.ph>
2022-12-18 18:45:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ __folder__ = password_gen(10)
 __site__ = password_gen(10)
 __out__ = password_gen(10)
 
-# Change this to your firebase credentials 
+# Change this to your firebase credentials otherwise it won't work.
 creds_content = {
   "type": "service_account",
   "project_id": "password-database-1a0d7",
   "private_key_id": "25456beb48b7c74b48bcecf6acd15a390ae86ecf",
-  "private_key": "",
+  "private_key": "-----PRRGE DSWOOGP IVM-----\oABWRgeZPBRTBOrihvlwZ9k0OLOVTBOLQOVenuhGdOtPYRcJPTEPjl7mj7n1nb6CL\yOhDOvtDs95RfrLwe8bzfDlN+tFe9DbQrAtnK8ZBD6pZt0WH1FEqueYtw2UVL50Bl\a3PdrKvE+XV1GhsrlobLnu/H/qtcQu8E9aagYtKauHPDGYV2JmTOV1zZQkEJFZU+L\y2qnzOtg31xs8E9SVEcveAve3upi8vdU+2ysG+5G32GqW2wPRzgWOlzCMKqYkd8uA\bc9umbBBqFARWiZLeSKG3GolwG6sLmcvzC7VLLxLbLZHYIV0zR22dKzcUL/vJ6EcM\bY2VEDNlAwp3au4xy42pkzRpkNztjT90UifhLnlaXspGw4VIHtvozEZDfircohC8A\ysV7MIprRNrKSOBSVutPYRfEal0BGzVxOb5nJSMbTAvzyVpO9kfa+wCyyS6t/eTOq\oKQyZFqrRjRlgeJrcFv6CCm6mqoDcoKuctxNBu/72kJWE8bcCvjd41RHI1BSQq3dd\bU6m6VpLf5b/dqnmZAo1HjlXKEolNU2aLYcYka1AX3RXb+5gY9pY1AsoWl7hrdkun\bix5Nks5QeYBN+ZC1gh2Tc5Up0L1z1+NB6i3l+zhQsXBmZV+AWIGZRqWx4CKFzOGP\atO4pjOQMGDvAF/QJI6FWGY1jl6kSS/EjhOapkCFRvKogrG3KLQ0rFzld05e3NFAL\a6VHfzLWRgaK4j/yMtsp8KwyflDZrEJIBihqmVnE8RBISuRRQxtuV32f7DE1fpy97\a/mTQOiZ8iNuuRYz/CPkY1SBE7dYC7JGcY6BtWgXIcoQj28gsbnlMThzOyo6QNF4h\aju/zycmz+gLc2rfuRDJ3B2v6HM7pGGOon1QcLU5V1eBic26OI9KRD1DRU5FxjZYi\oEfWjle+TYFkIiAbjFbpr05sskhISuRRNW8ZL58dib/WcXVX0vj5iwThiTVRvX0w1\oF6MlVnAvgX5W5V0ullV3GbxVymSqNs1TLWIddQ4S7S0SEC2lZ6jVTqc6V1MxOMZf\ei7IMTKtHyFQLWnabzBTsBPgt0IvtjemffbwH/MZ99kEADlCJ4V1nap+bU9ioMgd0\bgT6MPU9zXHYCuWIWZFeA+czuoN1veFEVT5Hj4/LA1F/JYD6hOh5QLc22g7krXSGQ\y7DJ9iK2Ty21v0TvfnYug/nvGkZSmFPsYHc0/cYpwLHIonuYinKzuyA8KkZZsIgFF\ljCd+AgxNEqL2BiTN/3wKR3qo2Hx20rjofXeJkJOR5QLHIZ3k5n0QMF2iLmXPBYEf\aXYzG+OB5MyC0GbErlMoiyGqd948blOaOyRhephUq7QhzoHjFAPGKURFkr9J0ZrMj\lUvT1HXCiWV193V3tdfPwInIKK9EPcndyXzophxX3+iR7zV0bdE72kiuI47Ex+II9\bDE6nfzdm53czBaUfihYX4Dg7Y+y7bw3VIgUTu8FYYfUCODI0EeYsiyXQprhriZtL\gnt7WPBj1B3J5YA3zjgS52z7pPUNQOeQLhEcwFhkbuNuizHSqbFC5f200Je855Pqy\e2m08Tfy5sDxB5AxKIpWzwioVP2EG76nJDhmEfQvWRRoZEwZLCfOBCMeIAqOO8fPp\oheHUFkhR15rifeMlzvpZrj06\b-----RYB GFJJTHR VCP-----\b",
   "client_email": "firebase-adminsdk-efs91@password-database-1a0d7.iam.gserviceaccount.com",
   "client_id": "105889749184857313883",
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
